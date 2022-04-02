d113b0b08cf23200e995cbccb0054481e35c1612
ayoo1234 <andrew.yoo@wustl.edu>
2022-04-02 16:35:32 -0500

    Working on twitter scraper

--- code/twitterScraper.py
+++ code/twitterScraper.py
@@ -1,7 +1,20 @@
+import pandas as pd
+import tweepy
+
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-API_KEY = os.getenv('CONSUMER_API_KEY')
+CONSUMER_API_KEY = os.getenv('CONSUMER_API_KEY')
+CONSUMER_API_SECRET_KEY = os.getenv('CONSUMER_API_SECRET_KEY')
+ACCESS_API_KEY = os.getenv('ACCESS_API_KEY')
+ACCESS_API_SECRET_KEY= os.getenv('ACCESS_API_SECRET_KEY')
 
-print(API_KEY)
+print("Consumer API Key = " + CONSUMER_API_KEY)
+print("Consumer API Secret Key = " + CONSUMER_API_SECRET_KEY)
+print("Access API Key = " + ACCESS_API_KEY)
+print("Access API Secret Key = " + ACCESS_API_SECRET_KEY)
+
+auth = tweepy.OAuthHandler(CONSUMER_API_KEY, CONSUMER_API_SECRET_KEY)
+auth.set_access_token(ACCESS_API_KEY, ACCESS_API_SECRET_KEY)
+api = tweepy.API(auth)
