16804cdc119bc031e0b9b729338d1c6c7585b5bf
zlzhao124 <zlzhao@wustl.edu>
2022-05-07 03:30:36 -0500

    callbacks working for test app

--- code/apptest.py
+++ code/apptest.py
@@ -1,9 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pandas as pd
 import json
 import plotly
 import plotly.express as px
 
+#SOURCES: https://towardsdatascience.com/web-visualization-with-plotly-and-flask-3660abf9c946
+#https://towardsdatascience.com/an-interactive-web-dashboard-with-plotly-and-flask-c365cdec5e3f 
+
+df = pd.read_csv(r'code/SentimentAnalysis/sentiment_df.csv')
+df = df[['date', 'sentiment', 'compound']]
+df2 = pd.DataFrame({
+    'Company': [],
+    'Date': [],
+    'Count Type': [],
+    'Count': [],
+    'Compound Average': []
+})
+entry = {'Company': 'Tesla', 'Date': '2022-04-30', 'Count Type': 'Positive', 'Count': df['sentiment'].value_counts()['positive'], 'Compound Average': df['compound'].mean()}
+df2 = df2.append(entry, ignore_index = True)
+entry = {'Company': 'Tesla', 'Date': '2022-04-30', 'Count Type': 'Negative', 'Count': df['sentiment'].value_counts()['negative'], 'Compound Average': df['compound'].mean()}
+df2 = df2.append(entry, ignore_index = True)
+
+#test company
+entry = {'Company': 'TestCompany', 'Date': '2022-04-30', 'Count Type': 'Positive', 'Count': 42, 'Compound Average': 0.5}
+df2 = df2.append(entry, ignore_index = True)
+entry = {'Company': 'TestCompany', 'Date': '2022-04-30', 'Count Type': 'Negative', 'Count': 69, 'Compound Average': 0.5}
+df2 = df2.append(entry, ignore_index = True)
+entry = {'Company': 'TestCompany', 'Date': '2022-04-29', 'Count Type': 'Positive', 'Count': 42, 'Compound Average': 0.4}
+df2 = df2.append(entry, ignore_index = True)
+entry = {'Company': 'TestCompany', 'Date': '2022-04-29', 'Count Type': 'Negative', 'Count': 69, 'Compound Average': 0.4}
+df2 = df2.append(entry, ignore_index = True)
+
+#test date
+entry = {'Company': 'Tesla', 'Date': '2022-04-29', 'Count Type': 'Positive', 'Count': 0, 'Compound Average': -0.2}
+df2 = df2.append(entry, ignore_index = True)
+entry = {'Company': 'Tesla', 'Date': '2022-04-29', 'Count Type': 'Negative', 'Count': 0, 'Compound Average': -0.2}
+df2 = df2.append(entry, ignore_index = True)
 #df.loc[df['column_name'] == some_value]
 
 app = Flask(__name__)
@@ -11,72 +43,34 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/posnegcount')
-def posneg():
-    df = pd.read_csv(r'code/SentimentAnalysis/sentiment_df.csv')
-    df = df[['date', 'sentiment', 'compound']]
-    df2 = pd.DataFrame({
-        'Company': [],
-        'Date': [],
-        'Count Type': [],
-        'Count': [],
-        'Compound Average': []
-    })
-    entry = {'Company': 'Tesla', 'Date': '2022-04-30', 'Count Type': 'Positive', 'Count': df['sentiment'].value_counts()['positive'], 'Compound Average': df['compound'].mean()}
-    df2 = df2.append(entry, ignore_index = True)
-    entry = {'Company': 'Tesla', 'Date': '2022-04-30', 'Count Type': 'Negative', 'Count': df['sentiment'].value_counts()['negative'], 'Compound Average': df['compound'].mean()}
-    df2 = df2.append(entry, ignore_index = True)
-    
-
-    #test company
-    entry = {'Company': 'TestCompany', 'Date': '2022-04-30', 'Count Type': 'Positive', 'Count': 42, 'Compound Average': 0.5}
-    df2 = df2.append(entry, ignore_index = True)
-    entry = {'Company': 'TestCompany', 'Date': '2022-04-30', 'Count Type': 'Negative', 'Count': 69, 'Compound Average': 0.5}
-    df2 = df2.append(entry, ignore_index = True)
-
-    #test date
-    entry = {'Company': 'Tesla', 'Date': '2022-04-29', 'Count Type': 'Positive', 'Count': 0, 'Compound Average': -0.2}
-    df2 = df2.append(entry, ignore_index = True)
-    entry = {'Company': 'Tesla', 'Date': '2022-04-29', 'Count Type': 'Negative', 'Count': 0, 'Compound Average': -0.2}
-    df2 = df2.append(entry, ignore_index = True)
-
-    fig = px.bar(df2, x='Company', y='Count', color='Count Type', barmode='group')
-    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-    return render_template('notdash.html', graphJSON=graphJSON)
-
-
 @app.route('/compavg')
 def compavg():
-    df = pd.read_csv(r'code/SentimentAnalysis/sentiment_df.csv')
-    df = df[['date', 'sentiment', 'compound']]
-    df2 = pd.DataFrame({
-        'Company': [],
-        'Date': [],
-        'Count Type': [],
-        'Count': [],
-        'Compound Average': []
-    })
-    entry = {'Company': 'Tesla', 'Date': '2022-04-30', 'Count Type': 'Positive', 'Count': df['sentiment'].value_counts()['positive'], 'Compound Average': df['compound'].mean()}
-    df2 = df2.append(entry, ignore_index = True)
-    entry = {'Company': 'Tesla', 'Date': '2022-04-30', 'Count Type': 'Negative', 'Count': df['sentiment'].value_counts()['negative'], 'Compound Average': df['compound'].mean()}
-    df2 = df2.append(entry, ignore_index = True)
+    return render_template('notdash2.html', graphJSON=callbacktest())
 
-    #test company
-    entry = {'Company': 'TestCompany', 'Date': '2022-04-30', 'Count Type': 'Positive', 'Count': 42, 'Compound Average': 0.5}
-    df2 = df2.append(entry, ignore_index = True)
-    entry = {'Company': 'TestCompany', 'Date': '2022-04-30', 'Count Type': 'Negative', 'Count': 69, 'Compound Average': 0.5}
-    df2 = df2.append(entry, ignore_index = True)
+@app.route('/callbacktest', methods=['POST', 'GET'])
+def cb():
+    return callbacktest(request.args.get('data'))
 
-    #test date
-    entry = {'Company': 'Tesla', 'Date': '2022-04-29', 'Count Type': 'Positive', 'Count': 0, 'Compound Average': -0.2}
-    df2 = df2.append(entry, ignore_index = True)
-    entry = {'Company': 'Tesla', 'Date': '2022-04-29', 'Count Type': 'Negative', 'Count': 0, 'Compound Average': -0.2}
-    df2 = df2.append(entry, ignore_index = True)
-
-    fig = px.line(df2, x='Date', y='Compound Average', title='Compound Average per Day for One Company')
+def callbacktest(company = 'Tesla'):
+    fig = px.line(df2[df2['Company']==company], x='Date', y='Compound Average', title='Compound Average per Day for One Company')
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-    return render_template('notdash.html', graphJSON=graphJSON)
+    #return render_template('notdash.html', graphJSON=graphJSON)
+    return graphJSON
 
+
+@app.route('/posnegcount')
+def posneg():
+    return render_template('notdash.html', graphJSON=callbacktest2())
+
+@app.route('/callbacktest2', methods=['POST', 'GET'])
+def cb1():
+    return callbacktest2(request.args.get('data'))
+
+def callbacktest2(date = '2022-04-30'):
+    fig = px.bar(df2[df2['Date']==date], x='Company', y='Count', color='Count Type', barmode='group')
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    #return render_template('notdash.html', graphJSON=graphJSON)
+    return graphJSON
 
 if __name__ == "__main__":
     app.run()
