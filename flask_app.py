9d86c6ec4723b379daa6fd7ec31ba49c5193253d
ayoo1234 <andrew.yoo@wustl.edu>
2022-05-09 16:14:56 -0500

    Save for now

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,10 @@
 from flask import Flask, flash, request, redirect, url_for, jsonify, render_template, Response
 
-
 app = Flask(__name__)
 
 @app.route("/")
 def index():
 	return render_template('index.html')
-
 
 @app.route("/test")
 def hello_world():
@@ -14,21 +12,25 @@ def hello_world():
 
 @app.route("/Andrew")
 def andrew():
-    
 	variable = 100
+    graphJSON=positiveNegativeCallBackTest()
 	return render_template('andrew.html', number=variable)
 
 @app.route("/Zach")
 def zach():
 	return render_template('zach.html')
 
-@app.route("/keith")
+@app.route("/Keith")
 def keith():
 	return render_template('keith.html')
 
-@app.route("/john")
+@app.route("/John")
 def john():
 	return render_template('john.html')
+    
+@app.route("/Hoang")
+def hoang():
+	return render_template('hoang.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
